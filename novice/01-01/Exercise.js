b7ebb133cf3cef8a0f1f60e08c02c0db05f6c7e4
ignadwi19 <ignadwi19@gmail.com>
2019-11-04 13:31:03 +0700

    first commit

---
+++ novice/01-01/Exercise.js
@@ -0,0 +1,14 @@
+function loop {
+
+for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n; j++) {
+        if(i === j) {
+            document.write("*"); }
+            else {
+                document.write("*");
+            }
+        }
+        printf("\n");
+    }
+}
+    console.log();
